d7323958ae98a36c0f5c08ad0ef12d75aa1bd027
DavidTrujillo123 <102611104+DavidTrujillo123@users.noreply.github.com>
2022-10-11 19:10:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // Paquetes
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 //middlewears
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(cors())
 
 //Puerto
 app.set("port", process.env.PORT || 3001)
